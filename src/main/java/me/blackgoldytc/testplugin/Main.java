f970d3638adb9a5ad12536afd704614d9fc38595
BuildTools <unconfigured@null.spigotmc.org>
2021-02-14 02:23:25 +0200

    first commit

---
+++ src/main/java/me/blackgoldytc/testplugin/Main.java
@@ -0,0 +1,63 @@
+package me.blackgoldytc.testplugin;
+
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.connection.PluginMessageEvent;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.ServerConnection;
+import com.velocitypowered.api.proxy.messages.LegacyChannelIdentifier;
+import com.velocitypowered.api.proxy.messages.MinecraftChannelIdentifier;
+import org.slf4j.Logger;
+
+import java.nio.file.Path;
+
+@Plugin(id = "testplugin", name = "testplugin", version = "1.0-SNAPSHOT")
+public class Main {
+
+    private final Path dataDirectory;
+    private final ProxyServer server;
+    private final Logger logger;
+
+    private static final LegacyChannelIdentifier LEGACY_BUNGEE_CHANNEL = new LegacyChannelIdentifier("BungeeCord");
+    private static final MinecraftChannelIdentifier MODERN_BUNGEE_CHANNEL = MinecraftChannelIdentifier.create("bungeecord", "main");
+
+    @Inject
+    public Main(ProxyServer server, Logger logger, @DataDirectory Path dataDirectory) {
+        this.server = server;
+        this.logger = logger;
+        this.dataDirectory = dataDirectory;
+
+    }
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        server.getChannelRegistrar().register(LEGACY_BUNGEE_CHANNEL, MODERN_BUNGEE_CHANNEL);
+    }
+
+    @Subscribe
+    public void onPluginMessage(PluginMessageEvent event) {
+        if (!event.getIdentifier().equals(LEGACY_BUNGEE_CHANNEL) && !event.getIdentifier().equals(MODERN_BUNGEE_CHANNEL)) {
+            return;
+        }
+
+        event.setResult(PluginMessageEvent.ForwardResult.handled());
+
+        if (!(event.getSource() instanceof ServerConnection)) {
+            return;
+        }
+        ByteArrayDataInput in = event.dataAsDataStream();
+        String subChannel = in.readUTF();
+
+        if (subChannel.equals("PlayerJoined")) {
+            logger.info("Player " + in.readUTF() + "Has joined the server and have received the title " + in.readUTF());
+        }
+    }
+
+
+    }
